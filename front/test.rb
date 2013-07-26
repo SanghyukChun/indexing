000631a7202869db841c9ed76cf494d43e9122b3
sanghyuk <sanghyuk.chun@gmail.com>
2013-07-26 15:40:22 +0900

    test cmd_parser ver2

---
+++ front/test.rb
@@ -0,0 +1,39 @@
+require 'readline'
+require 'time'
+require 'ipaddr'
+
+class String
+	def is_valid_query?
+		return true
+	end
+end
+
+puts "==========================================="
+puts "========    flosis query client    ========"
+puts "==========================================="
+puts ""
+
+cmds = {}
+%w(stime etime src_ip dst_ip src_port dst_port bpf).each do |opt|
+	line = Readline.readline("#{opt}: ", true)
+	case opt.to_sym
+	when :stime, :etime
+		cmd = Time.parse(line).strftime("%s") #change format
+	when :src_ip, :dst_ip
+		ip = IPAddr.new(line)
+		abort "only ipv4 is available" unless ip.ipv4?
+		cmd = [ip.to_range.first.to_i, ip.to_range.last.to_i]
+	when :src_port, :dst_port
+		abort "only integer is available" unless line.match(/^\d+$/)
+		abort "invalid port" unless (0..65535).include? line.to_i
+		cmd = line.to_i
+	when :bpf
+		abort "invalid query" unless line.is_valid_query?
+		cmd = line
+	else
+		abort "wrong options"
+	end
+	cmds[opt.to_sym] = cmd
+end
+
+puts cmds.to_s
