5d18cb7f291c131c6927187034cd8154d08a04e7
sanghyuk <sanghyuk.chun@gmail.com>
2013-07-26 15:57:41 +0900

    override string and ipaddr for simplify code

--- front/test.rb
+++ front/test.rb
@@ -3,8 +3,20 @@ require 'time'
 require 'ipaddr'
 
 class String
-	def is_valid_query?
+	def int?
+		return self.match(/^\d+$/)
+	end
+	def valid_query?
 		return true
+	end
+end
+
+class IPAddr
+	def first
+		return self.to_range.first.to_i
+	end
+	def last
+		return self.to_range.last.to_i
 	end
 end
 
@@ -22,13 +34,13 @@ cmds = {}
 	when :src_ip, :dst_ip
 		ip = IPAddr.new(line)
 		abort "only ipv4 is available" unless ip.ipv4?
-		cmd = [ip.to_range.first.to_i, ip.to_range.last.to_i]
+		cmd = [ip.first, ip.last]
 	when :src_port, :dst_port
-		abort "only integer is available" unless line.match(/^\d+$/)
+		abort "only integer is available" unless line.int?
 		abort "invalid port" unless (0..65535).include? line.to_i
 		cmd = line.to_i
 	when :bpf
-		abort "invalid query" unless line.is_valid_query?
+		abort "invalid query" unless line.valid_query?
 		cmd = line
 	else
 		abort "wrong options"
