9adab1cd11b4bba790fa2cfd76a89102125e7578
sanghyuk <sanghyuk@fjord3.kaist.ac.kr>
2013-08-07 20:55:58 +0900

    edit index.c

--- index_test/index.c
+++ index_test/index.c
@@ -9,17 +9,19 @@
 #include "index.h"
 #include "hashes.h"
 
+/* TODO change define values */
 /*#define ARRAY_SIZE 40000*/
 /*#define FILE_PER_INDEXER 1000*/
-#define ARRAY_SIZE 40
-#define FILE_PER_INDEXER 3
+#define ARRAY_SIZE 4
+#define FILE_PER_INDEXER 10
 #define FILTER_SIZE 20
 #define FILTER_SIZE_BYTES (1 << (FILTER_SIZE - 3))
 #define FILTER_BITMASK ((1 << FILTER_SIZE) - 1)
 #define NUM_HASHES 7
 
 /*****************************************************************************/
-void sort_array(indexer_context_t *ictx)
+static inline void
+sort_array(indexer_context_t *ictx)
 {
 	index_array_t *ia = &ictx->ic_index[ictx->ic_array_idx];
 	QSORT(struct array_node, ia->saddr, ia->cnt, COMPARE_VALUE);
@@ -28,7 +30,8 @@ void sort_array(indexer_context_t *ictx)
 	QSORT(struct array_node, ia->dport, ia->cnt, COMPARE_VALUE);
 }
 /*****************************************************************************/
-void convert_into_char(unsigned char *char_data, unsigned int data)
+static inline void
+convert_into_char(unsigned char *char_data, unsigned int data)
 {
 	char_data[0] = (data >> 24) & 0xFF;
 	char_data[1] = (data >> 16) & 0xFF;
@@ -36,7 +39,8 @@ void convert_into_char(unsigned char *char_data, unsigned int data)
 	char_data[3] = (data)       & 0xFF;
 }
 /*****************************************************************************/
-void get_hashes(unsigned int hash[], unsigned char *data)
+static inline void
+get_hashes(unsigned int hash[], unsigned char *data)
 {
 	hash[0] = RSHash  (data, 4);
 	hash[1] = DJBHash (data, 4);
@@ -52,7 +56,8 @@ void get_hashes(unsigned int hash[], unsigned char *data)
 
 
 /*****************************************************************************/
-void init_bloom_filter(index_array_t *ia)
+static inline void
+init_bloom_filter(index_array_t *ia)
 {
 	bloom_filter_t *bf = (bloom_filter_t *)malloc(sizeof(bloom_filter_t));
 
@@ -64,7 +69,8 @@ void init_bloom_filter(index_array_t *ia)
 	ia->filter = bf;
 }
 /*****************************************************************************/
-void init_index_array(indexer_context_t *ictx, index_array_t *ia)
+static inline void
+init_index_array(indexer_context_t *ictx, index_array_t *ia)
 {
 	ia = (index_array_t *)calloc(FILE_PER_INDEXER, sizeof(index_array_t));
 
@@ -77,10 +83,12 @@ void init_index_array(indexer_context_t *ictx, index_array_t *ia)
 	ia[0].daddr = (array_node_t *)calloc(ARRAY_SIZE * FILE_PER_INDEXER, sizeof(array_node_t *));
 	ia[0].sport = (array_node_t *)calloc(ARRAY_SIZE * FILE_PER_INDEXER, sizeof(array_node_t *));
 	ia[0].dport = (array_node_t *)calloc(ARRAY_SIZE * FILE_PER_INDEXER, sizeof(array_node_t *));
+	ia[0].cnt   = 0;
 	ictx->ic_index = ia;
 }
 /*****************************************************************************/
-void init_indexer_context(indexer_context_t *ictx)
+inline void
+init_indexer_context(indexer_context_t *ictx)
 {
 	memset(ictx, 0, sizeof(indexer_context_t));
 	index_array_t *ia;
@@ -95,7 +103,8 @@ void init_indexer_context(indexer_context_t *ictx)
 
 
 /*****************************************************************************/
-void insert_into_bloom_filter(unsigned char *filter, unsigned int data)
+static inline void
+insert_into_bloom_filter(unsigned char *filter, unsigned int data)
 {
 	unsigned char char_data[4];
 	convert_into_char(char_data, data);
@@ -105,11 +114,12 @@ void insert_into_bloom_filter(unsigned char *filter, unsigned int data)
 	int i;
 	for (i=0; i<NUM_HASHES; i++) {
 		hash[i] = (hash[i] >> FILTER_SIZE) ^ (hash[i] & FILTER_BITMASK);
-		filter[hash[i] >> 3] |=1 << (hash[i] & 7);
+		filter[hash[i] >> 3] |= 1 << (hash[i] & 7);
 	}
 }
 /*****************************************************************************/
-void insert_into_index_array(index_array_t *ia, array_node_t *head, unsigned int data, FlowMeta *meta)
+static inline void
+insert_into_index_array(index_array_t *ia, array_node_t *head, unsigned int data, FlowMeta *meta)
 {
 	array_node_t *node = &head[ia->cnt];
 	node->value = data;
@@ -117,8 +127,10 @@ void insert_into_index_array(index_array_t *ia, array_node_t *head, unsigned int
 	node->offset = meta->offset;
 }
 /*****************************************************************************/
-void insert_index(indexer_context_t *ictx, FlowMeta *meta)
+static inline void
+insert_index(indexer_context_t *ictx, FlowMeta *meta)
 {
+	/* insert index into index array */
 	index_array_t *ia = &ictx->ic_index[ictx->ic_array_idx];
 	insert_into_index_array(ia, ia->saddr, meta->flowinfo.saddr, meta);
 	insert_into_index_array(ia, ia->daddr, meta->flowinfo.daddr, meta);
@@ -126,6 +138,7 @@ void insert_index(indexer_context_t *ictx, FlowMeta *meta)
 	insert_into_index_array(ia, ia->dport, meta->flowinfo.dport, meta);
 	ia->cnt += 1;
 
+	/* insert index into bloom filter */
 	bloom_filter_t *bf = ia->filter;
 	insert_into_bloom_filter(bf->saddr, meta->flowinfo.saddr);
 	insert_into_bloom_filter(bf->daddr, meta->flowinfo.daddr);
@@ -133,7 +146,8 @@ void insert_index(indexer_context_t *ictx, FlowMeta *meta)
 	insert_into_bloom_filter(bf->dport, meta->flowinfo.dport);
 }
 /*****************************************************************************/
-void create_index(indexer_context_t *ictx, FlowMeta *meta_block, int size)
+inline void
+create_index(indexer_context_t *ictx, FlowMeta *meta_block, int size)
 {
 	int i;
 	for (i=0; i<size; i++) {
@@ -146,7 +160,8 @@ void create_index(indexer_context_t *ictx, FlowMeta *meta_block, int size)
 
 
 /*****************************************************************************/
-int search_filter(unsigned char *filter, unsigned int data)
+static inline int
+search_filter(unsigned char *filter, unsigned int data)
 {
 	unsigned char char_data[4];
 	convert_into_char(char_data, data);
@@ -162,7 +177,8 @@ int search_filter(unsigned char *filter, unsigned int data)
 	return 1;
 }
 /*****************************************************************************/
-int search_from_bloom_filter(bloom_filter_t *filter, int type, unsigned int data)
+static inline int
+search_from_bloom_filter(bloom_filter_t *filter, int type, unsigned int data)
 {
 	int res = -1;
 	switch(type) {
@@ -189,7 +205,8 @@ int search_from_bloom_filter(bloom_filter_t *filter, int type, unsigned int data
 		return 0;
 }
 /*****************************************************************************/
-int binary_search(array_node_t *head, int start, int end, unsigned int data, int type)
+static inline int
+binary_search(array_node_t *head, int start, int end, unsigned int data, int type)
 {
 	if (start > end) {
 		switch(type) {
@@ -208,6 +225,8 @@ int binary_search(array_node_t *head, int start, int end, unsigned int data, int
 	int mid = (start + end) / 2;
 	unsigned int mid_val = head[mid].value;
 
+	printf("s: %d, e: %d, data: %u, midval: %u\n", start, end, data, mid_val);
+
 	if (mid_val > data)
 		return binary_search(head, start, mid-1, data, type);
 	else if (mid_val < data)
@@ -216,7 +235,9 @@ int binary_search(array_node_t *head, int start, int end, unsigned int data, int
 	return mid;
 }
 /*****************************************************************************/
-int search_backward(array_node_t *head, int idx, unsigned int data) {
+static inline int
+search_backward(array_node_t *head, int idx, unsigned int data)
+{
 	if (idx == -1)
 		return -1;
 
@@ -226,7 +247,9 @@ int search_backward(array_node_t *head, int idx, unsigned int data) {
 	return idx + 1;
 }
 /*****************************************************************************/
-int search_forward(array_node_t *head, int idx, unsigned int data, int size) {
+static inline int
+search_forward(array_node_t *head, int idx, unsigned int data, int size)
+{
 	if (idx == -1)
 		return -1;
 
@@ -236,31 +259,41 @@ int search_forward(array_node_t *head, int idx, unsigned int data, int size) {
 	return idx - 1;
 }
 /*****************************************************************************/
-void search_single_index(indexer_context_t *ictx, int type, unsigned int val, int res[])
+static inline void
+search_single_index(indexer_context_t *ictx, int type, unsigned int val, int res[])
 {
 	int idx, min_idx, max_idx;
 	index_array_t *ia = &ictx->ic_index[ictx->ic_array_idx];
 
 	switch(type) {
 		case TYPE_SADDR:
-			idx     = binary_search  (ia->saddr, 0, ia->cnt, val, SEARCH_EXACT);
-			max_idx = search_forward (ia->saddr, idx, val, ia->cnt);
-			min_idx = search_backward(ia->saddr, idx, val);
+			if (search_from_bloom_filter(ia->filter, TYPE_SADDR, val)) {
+				idx     = binary_search  (ia->saddr, 0, ia->cnt, val, SEARCH_EXACT);
+				max_idx = search_forward (ia->saddr, idx, val, ia->cnt);
+				min_idx = search_backward(ia->saddr, idx, val);
+			}
 			break;
 		case TYPE_DADDR:
-			idx     = binary_search  (ia->daddr, 0, ia->cnt, val, SEARCH_EXACT);
-			max_idx = search_forward (ia->daddr, idx, val, ia->cnt);
-			min_idx = search_backward(ia->daddr, idx, val);
+			if (search_from_bloom_filter(ia->filter, TYPE_DADDR, val)) {
+				idx     = binary_search  (ia->daddr, 0, ia->cnt, val, SEARCH_EXACT);
+				max_idx = search_forward (ia->daddr, idx, val, ia->cnt);
+				min_idx = search_backward(ia->daddr, idx, val);
+			}
 			break;
 		case TYPE_SPORT:
-			idx     = binary_search  (ia->sport, 0, ia->cnt, val, SEARCH_EXACT);
-			max_idx = search_forward (ia->sport, idx, val, ia->cnt);
-			min_idx = search_backward(ia->sport, idx, val);
+			if (search_from_bloom_filter(ia->filter, TYPE_SPORT, val)) {
+				idx     = binary_search  (ia->sport, 0, ia->cnt, val, SEARCH_EXACT);
+				max_idx = search_forward (ia->sport, idx, val, ia->cnt);
+				min_idx = search_backward(ia->sport, idx, val);
+			}
 			break;
 		case TYPE_DPORT:
-			idx     = binary_search  (ia->dport, 0, ia->cnt, val, SEARCH_EXACT);
-			max_idx = search_forward (ia->dport, idx, val, ia->cnt);
-			min_idx = search_backward(ia->dport, idx, val);
+			if (search_from_bloom_filter(ia->filter, TYPE_DPORT, val)) {
+				idx     = binary_search  (ia->dport, 0, ia->cnt, val, SEARCH_EXACT);
+				printf("idx: %d\n", idx);
+				max_idx = search_forward (ia->dport, idx, val, ia->cnt);
+				min_idx = search_backward(ia->dport, idx, val);
+			}
 			break;
 		default:
 			fprintf(stderr, "Unknown type\n");
@@ -271,8 +304,15 @@ void search_single_index(indexer_context_t *ictx, int type, unsigned int val, in
 	res[1] = max_idx;
 }
 /*****************************************************************************/
-void search_index(indexer_context_t *ictx, int type, unsigned int min_val, unsigned int max_val, int res[])
+inline void
+search_index(indexer_context_t *ictx, int type, unsigned int min_val, unsigned int max_val, int res[])
 {
+	/* README usage
+	 * int res[2];
+	 * search_index(ictx, TYPE_SADDR, min_val, max_val, res);
+	 * int min_idx = res[0];
+	 * int max_idx = res[1];
+	 * */
 	if (min_val > max_val)
 		return;
 	else if (min_val == max_val)
@@ -313,7 +353,8 @@ void search_index(indexer_context_t *ictx, int type, unsigned int min_val, unsig
 
 // TODO rename function
 /*****************************************************************************/
-void get_next_file(indexer_context_t *ictx)
+static inline void
+get_next_file(indexer_context_t *ictx)
 {
 	//TODO assign fileID to bf, ia
 	sort_array(ictx);
@@ -327,6 +368,7 @@ void get_next_file(indexer_context_t *ictx)
 	ictx->ic_array_idx += 1;
 	ia = &ictx->ic_index[ictx->ic_array_idx];
 
+	ia->cnt   = 0;
 	ia->saddr = saddr;
 	ia->daddr = daddr;
 	ia->sport = sport;
@@ -337,29 +379,7 @@ void get_next_file(indexer_context_t *ictx)
 
 
 
-/*****************************************************************************/
-void free_indexer(indexer_context_t *ictx)
-{
-	free(ictx->ic_index[0].saddr);
-	free(ictx->ic_index[0].daddr);
-	free(ictx->ic_index[0].sport);
-	free(ictx->ic_index[0].dport);
-
-	int i;
-	index_array_t *ia = ictx->ic_index;
-	for (i=0; i<FILE_PER_INDEXER; i++) {
-		free(ia[i].filter->saddr);
-		free(ia[i].filter->daddr);
-		free(ia[i].filter->sport);
-		free(ia[i].filter->dport);
-		free(ia[i].filter);
-	}
-
-	free(ia);
-	free(ictx);
-}
-/*****************************************************************************/
-
+/* TODO delete sample program*/
 int main()
 {
 	indexer_context_t tmp;
@@ -369,21 +389,26 @@ int main()
 
 #include <time.h>
 	srand(time(NULL));
-	int i,j,k;
+	int i,j;
 	FlowMeta *meta = (FlowMeta *)malloc(sizeof(FlowMeta));
 	FlowInfo *info = &meta->flowinfo;
 
+	unsigned int value;
 	for (i=0;i<FILE_PER_INDEXER;i++) {
 		for (j=0;j<ARRAY_SIZE;j++) {
 			info->saddr = rand();
 			info->daddr = rand();
 			info->sport = rand();
 			info->dport = rand();
+			value = info->saddr;
 
 			printf("%u\n", info->saddr);
 			insert_index(ictx, meta);
 		}
-		get_next_file(ictx);
+		if (i != FILE_PER_INDEXER-1)
+			get_next_file(ictx);
+		else
+			sort_array(ictx);
 	}
 	index_array_t *ia;
 	array_node_t *head;
@@ -399,6 +424,16 @@ int main()
 		/*}*/
 	/*}*/
 
+	/*head = ictx->ic_index[1].saddr;*/
+	/*unsigned char *bitmap;*/
+	/*for (i=0;i<FILE_PER_INDEXER;i++) {*/
+		/*ia = &ictx->ic_index[i];*/
+		/*bitmap = ia->filter->saddr;*/
+		/*for (j=0;j<FILTER_SIZE_BYTES;j++) {*/
+			/*printf("%d",bitmap[j]);*/
+		/*}*/
+	/*}*/
+
 	/*for (i=0;i<FILE_PER_INDEXER;i++) {*/
 		/*ia = &ictx->ic_index[i];*/
 		/*ia->saddr = head;*/
@@ -408,6 +443,17 @@ int main()
 		/*}*/
 		/*head = &index_array[j];*/
 	/*}*/
+
+
+	printf("=========================\n");
+	printf("value: %u\n", value);
+	int res[2];
+	search_index(ictx, TYPE_SADDR, value, value, res);
+	/*search_index(ictx, TYPE_SADDR, 0, value, res);*/
+	int min_idx = res[0];
+	int max_idx = res[1];
+
+	printf("min: %d, max:%d\n", min_idx, max_idx);
 
 	printf("=========================\n");
 	head = ictx->ic_index[0].saddr;
@@ -420,4 +466,5 @@ int main()
 		}
 		head = &index_array[j];
 	}
+	return 0;
 }
